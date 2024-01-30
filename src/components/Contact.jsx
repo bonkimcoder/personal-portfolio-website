113e650b231e0bed428faee08682a38c93492f42
Boniface Kim <bonkim.wambo@gmail.com>
2024-01-30 18:09:11 +0300

    added features to counter and contact components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,53 +3,55 @@ import email from "../assets/email.png";
 import phone from "../assets/phone.png";
 import shape from "../assets/shape.png";
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { useEffect } from "react";
+import { useState, useRef } from "react";
 const Contact = () => {
-  useEffect(() => {
-    const inputs = document.querySelectorAll(".container > *:first-child");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    function focusFunc() {
-      let parent = this.parentNode;
-      parent.classList.add("focus");
-    }
-
-    function blurFunc() {
-      let parent = this.parentNode;
-      if (this.value === "") {
-        parent.classList.remove("focus");
-      }
-    }
-
-    inputs.forEach((input) => {
-      input.addEventListener("focus", focusFunc);
-      input.addEventListener("blur", blurFunc);
-    });
-
-    return () => {
-      // Cleanup: Remove event listeners when the component is unmounted
-      inputs.forEach((input) => {
-        input.removeEventListener("focus", focusFunc);
-        input.removeEventListener("blur", blurFunc);
-      });
-    };
-  }, []);
+  const nameInput = useRef();
+  const emailInput = useRef(null);
+  const phoneInput = useRef(null);
+  const messageInput = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (
+      nameInput.current.value &&
+      emailInput.current.value &&
+      phoneInput.current.value &&
+      messageInput.current.value
+    ) {
+      setIsSubmitted(!isSubmitted);
+    }
+  };
+
+  const [focusState, setFocusState] = useState({
+    name: false,
+    email: false,
+    phone: false,
+    message: false,
+  });
+
+  const handleFocus = (inputName) => {
+    setFocusState((prevFocusState) => ({
+      ...prevFocusState,
+      [inputName]: true,
+    }));
+  };
+
+  const handleBlur = (inputName, inputValue) => {
+    setFocusState((prevFocusState) => ({
+      ...prevFocusState,
+      [inputName]: inputValue.trim() !== "",
+    }));
   };
 
   return (
     <div className="max-w-4xl my-12 mx-auto">
       <div className="relative flex items-center justify-center">
         <span className="absolute w-500 h-500 bg-gradient-to-b from-teal-400 to-teal-800 rounded-full bottom-1/2 right-1/2 transform translate-x-1/3 translate-y-2/5"></span>
-        <img
-          src={shape}
-          className="absolute h-80 top-1/2 left-1/2 transform -translate-x-2/3 -translate-y-1/2 opacity-20"
-          alt="Shape"
-        />
         <div className="flex flex-col sm:flex-row w-full max-w-4xl bg-white rounded-md shadow-lg overflow-hidden gap-2 ">
-          {/* Contact Info */}
-          <div className="p-8 sm:w-1/2 w-full">
+          <div className="p-8 sm:w-1/2 w-full flex flex-col gap-4">
             <h3 className="text-2xl font-semibold text-teal-500">
               Let's get in touch
             </h3>
@@ -103,33 +105,55 @@ const Contact = () => {
           </div>
 
           {/* Contact Form */}
-          <div className="relative bg-teal-700 p-8 sm:w-1/2 w-full">
+          <div
+            className={`${
+              isSubmitted ? "hidden" : ""
+            }  relative bg-teal-700 p-8 sm:w-1/2 w-full`}
+          >
             <span className="absolute w-130 h-130 bg-gradient-to-b from-transparent via-transparent to-teal-600 rounded-full top-20 right-1/3"></span>
             <span className="absolute w-80 h-80 bg-gradient-to-b from-transparent via-transparent to-teal-600 rounded-full top-2 right-1/6"></span>
             <form className="relative" onSubmit={handleSubmit}>
               <h3 className="text-2xl font-semibold text-white mb-4">
                 Talk To Me
               </h3>
-              <div className="container relative my-8">
+              <div
+                className={`${
+                  focusState.name ? "focus" : ""
+                } container relative my-8`}
+              >
                 <input
                   type="text"
                   name="name"
+                  required
+                  ref={nameInput}
+                  onFocus={() => handleFocus("name")}
+                  onBlur={(e) => handleBlur("name", e.target.value)}
+                  autoComplete="new-name"
                   className="w-full outline-none border-2 border-gray-100 bg-transparent px-3 py-2 text-white font-semibold text-sm leading-tight tracking-wide rounded-lg transition duration-300"
                 />
                 <label
                   htmlFor="name"
                   className="absolute top-1/2 left-4 transform -translate-y-1/2 px-1/10 text-white text-sm font-normal pointer-events-none transition-all duration-500 "
                 >
-                  Username
+                  Your Name
                 </label>
                 <span className="absolute top-0 left-25 transform -translate-y-1/2 text-transparent text-xs md:text-sm px-1 py-0.4 pointer-events-none">
-                  Usernam
+                  Your Name
                 </span>
               </div>
-              <div className="container relative my-8">
+              <div
+                className={`container relative my-8 ${
+                  focusState.email ? "focus" : ""
+                }`}
+              >
                 <input
                   type="email"
                   name="email"
+                  required
+                  ref={emailInput}
+                  onFocus={() => handleFocus("email")}
+                  onBlur={(e) => handleBlur("email", e.target.value)}
+                  autoComplete="new-email"
                   className="w-full outline-none border-2 border-gray-100 bg-transparent px-3 py-2 text-white font-semibold text-sm leading-tight tracking-wide rounded-lg transition duration-300"
                 />
                 <label
@@ -142,10 +166,18 @@ const Contact = () => {
                   Email
                 </span>
               </div>
-              <div className="container relative my-8">
+              <div
+                className={`container relative my-8 ${
+                  focusState.phone ? "focus" : ""
+                }`}
+              >
                 <input
                   type="tel"
                   name="phone"
+                  required
+                  ref={phoneInput}
+                  onFocus={() => handleFocus("phone")}
+                  onBlur={(e) => handleBlur("phone", e.target.value)}
                   className="w-full outline-none border-2 border-gray-100 bg-transparent px-3 py-2 text-white font-semibold text-sm leading-tight tracking-wide rounded-lg transition duration-300"
                 />
                 <label
@@ -158,9 +190,17 @@ const Contact = () => {
                   Phone
                 </span>
               </div>
-              <div className="container relative mt-8 mb-4">
+              <div
+                className={`container relative mt-8 mb-4 ${
+                  focusState.message ? "focus" : ""
+                }`}
+              >
                 <textarea
                   name="message"
+                  required
+                  ref={messageInput}
+                  onFocus={() => handleFocus("message")}
+                  onBlur={(e) => handleBlur("message", e.target.value)}
                   className=" 
                   w-full outline-none border-2 border-gray-100 bg-transparent py-3 px-5 text-white font-semibold text-sm leading-tight tracking-wide transition duration-300 min-h-40 rounded-lg resize-none overflow-y-auto"
                 ></textarea>
@@ -177,9 +217,19 @@ const Contact = () => {
               <input
                 type="submit"
                 value="Send"
-                className="w-full p-2.5 px-5 bg-transparent border-2 border-gray-100 text-green-500 text-base leading-none rounded-xl outline-none cursor-pointer transition duration-300 ease-in-out m-0 hover:bg-transparent hover:text-white active:scale-95"
+                className="w-full p-2.5 px-5 bg-transparent border-2 border-gray-100 text-white text-base leading-none rounded-xl outline-none cursor-pointer transition duration-300 ease-in-out m-0 hover:bg-teal-600 hover:text-white active:scale-95"
               />
             </form>
+          </div>
+          <div
+            className={`${
+              isSubmitted ? "" : "hidden"
+            } relative bg-teal-700 p-8 sm:w-1/2 w-full  flex justify-center items-center text-white`}
+          >
+            <h2 className="text-center text-2xl">
+              Thank you for your message. I will get back to you as soon as
+              possible
+            </h2>
           </div>
         </div>
       </div>
